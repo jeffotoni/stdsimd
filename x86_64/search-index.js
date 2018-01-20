1d1f20e1981c17181cf2c9208f2dcf6bf5270bff
Travis CI User <travis@example.org>
2018-01-20 18:31:37 +0000

    Update documentation

---
+++ x86_64/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["stdsimd"] = {"doc":"SIMD and vendor intrinsics support library.","items":[[0,"vendor","stdsimd","Platform dependent vendor intrinsics.",null,null],[0,"simd","","Platform independent SIMD vector types and operations.",null,null],[14,"cfg_feature_enabled","","Is a feature supported by the host CPU?",null,null]],"paths":[]};
+initSearch(searchIndex);
